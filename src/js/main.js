e42d0559b48fb7fdbf49ab38cdad30674f9b1490
Valts Vilcans <valts03@inbox.lv>
2021-03-04 12:20:18 +0100

    Slider base

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,20 @@ import $ from "jquery";
 import "popper.js";
 import "bootstrap";
 
+// import Swiper JS/styles
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+// Swiper navigation
+import SwiperCore, { Navigation} from 'swiper/core';
+SwiperCore.use([Navigation]);
+
+const swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });;
+
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
